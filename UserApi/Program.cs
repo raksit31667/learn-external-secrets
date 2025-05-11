e54dcda56d497728a33371d52232b89c06a3fc37
raksit31667 <raksit.m@ku.th>
2025-05-11 19:45:01 +0700

    👔 add user API service having an endpoint to get secrets from env variables

---
+++ UserApi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add configuration from environment
+builder.Configuration.AddEnvironmentVariables();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/secrets", (IConfiguration config) =>
+{
+    var username = config["MY_USERNAME"];
+    var password = config["MY_PASSWORD"];
+    return $"Secret from External Secret: username={username} password={password}";
+})
+.WithName("GetSecrets")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
